6e39eae62248367e739405fde47082d816af283e
ldtpvince <ledangthienphuc@gmail.com>
2021-12-02 10:35:12 +0700

    Revert "add event handler"
    This reverts commit 2f510615369dc1cb5417067575808d8a37394a0f.

--- source/config.py
+++ source/config.py
@@ -1,8 +1,3 @@
-import numpy as np
-
-LOGGER = [("game-debug", "./logs"), ("game-view", "./logs"), ("game-controller", "./logs"),
-          ("game-modeimport numpy as np
-
 LOGGER = [("game-debug", "./logs"), ("game-view", "./logs"), ("game-controller", "./logs"),
           ("game-model", "./logs"),
           ("game-socket", "./logs")]
@@ -10,18 +5,3 @@ MAP_WIDTH = 32
 MAP_HEIGHT = 16
 WIN_WIDTH = 1024
 WIN_HEIGHT = 768
-
-STOP = 0
-UP = 1
-DOWN = -1
-LEFT = 2
-RIGHT = -2
-SHOOT = 3
-
-DIRECTIONS = {
-    STOP: np.array([0, 0]), 
-    UP: np.array([0, -1]), 
-    DOWN: np.array([0, 1]), 
-    LEFT: np.array([-1, 0]), 
-    RIGHT: np.array([1, 0])
-}
