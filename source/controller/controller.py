e6699be1b1cf1f63fd1785c3400715d57e9de9e9
Nguyen Dinh Hoang Phuc <ndhp2000@gmail.com>
2021-12-12 17:12:13 +0700

    abcd

--- source/controller/controller.py
+++ source/controller/controller.py
@@ -1,3 +1,4 @@
+import random
 from sys import exit
 
 import pygame
@@ -5,12 +6,17 @@ from pygame.locals import *
 
 from source.config import *
 from ..model.model import MainGameLogic
+from ..network.network import GameNetwork
 from ..view.view import MainGameView
 
 
 class Controller:
-    def __init__(self):
+    COOLDOWN_COMMAND = 30
+
+    def __init__(self, is_auto_play=False):
         self._clock = pygame.time.Clock()
+        self._is_bot_player = is_auto_play
+        self._counter = self.COOLDOWN_COMMAND
 
         # Init Logic
         self._logic_ = MainGameLogic()
@@ -26,6 +32,9 @@ class Controller:
         self._view_.init_players(self._logic_.get_players())
         self._view_.init_bullets(self._logic_.get_bullets())
 
+        # Init Network
+        # self._network = GameNetwork()
+
     @staticmethod
     def get_event(key_pressed):
         if key_pressed == pygame.K_UP:
@@ -40,6 +49,21 @@ class Controller:
             return SHOOT
         return None
 
+    def _event_generator(self):  # Not used now
+        """
+        Create random event for bot player
+        :return: event
+        """
+        self._counter -= 1
+        if self._counter == 0:
+            self._counter = self.COOLDOWN_COMMAND
+            rand_num = random.randint(-N_TYPE_COMMANDS, N_TYPE_COMMANDS - 1)  # TODO UPDATE FOR BACKEND
+            if rand_num < 0:
+                return SHOOT
+            else:
+                return list(PLAYER_MOVEMENT.keys())[rand_num - 1]
+        return None
+
     def loop(self):
         while True:
             dt = self._clock.tick(60) / 1000
@@ -50,7 +74,14 @@ class Controller:
                     pygame.quit()
                     exit()
                 if event.type == pygame.KEYDOWN:
-                    input_event = self.get_event(event.key)
+                    if self._is_bot_player:
+                        input_event = self._event_generator()
+                    else:
+                        input_event = self.get_event(event.key)
+                # TODO: Send packet to server
+
+            # events = self._network.receive(PROCESSED_EVENTS_PER_LOOPS)
+            # self._logic_.update(events, dt)
 
             self._logic_.update(input_event, dt)
 
